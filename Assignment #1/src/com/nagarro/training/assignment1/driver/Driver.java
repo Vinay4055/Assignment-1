5cff14dc086c197422b7052ed3d11b8a40012902
vinayprajapati <vinayprajapati@vinayprajapati.Nagarro.local>
2020-01-14 11:36:04 +0530

    More Exceptions Handled

--- Assignment #1/src/com/nagarro/training/assignment1/driver/Driver.java
+++ Assignment #1/src/com/nagarro/training/assignment1/driver/Driver.java
@@ -71,7 +71,7 @@ public class Driver {
 		   input=sc.nextLine();
 		   }
 		   else { 
-			   System.out.println("Invalid Information");
+			   System.out.println("Invalid Information,Please Check Your Input");
 		   	   System.out.println("The Input Should Be in the form\n"
 					+ "-name item_name "
 					+ "-price item_price "
